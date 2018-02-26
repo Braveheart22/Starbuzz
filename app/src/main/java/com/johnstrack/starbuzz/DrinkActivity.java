58e3cedc75855d6c38bc78bc1da34770410ae854
John Strack <Braveheart22@GMail.com>
2018-02-26 15:32:54 -0500

    Create Drink Activity

---
+++ app/src/main/java/com/johnstrack/starbuzz/DrinkActivity.java
@@ -0,0 +1,13 @@
+package com.johnstrack.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrinkActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+    }
+}
