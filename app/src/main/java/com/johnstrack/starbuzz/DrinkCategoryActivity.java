5793d5c0c4e9edee30d42e0f89b8954e4a4dc0a4
John Strack <Braveheart22@GMail.com>
2018-02-26 15:16:43 -0500

    Populate DrinkCategoryActivity

--- app/src/main/java/com/johnstrack/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/johnstrack/starbuzz/DrinkCategoryActivity.java
@@ -2,6 +2,8 @@ package com.johnstrack.starbuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinkCategoryActivity extends Activity {
 
@@ -9,5 +11,8 @@ public class DrinkCategoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> lisrAdapter = new ArrayAdapter<(this, android.R.layout.simple_list_item_1, Drink.drinks);
+        ListView listDrinks = findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(lisrAdapter);
     }
 }
