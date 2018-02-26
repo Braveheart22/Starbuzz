3dc77baa54e7535993dcadc4ef975f76065717d4
John Strack <Braveheart22@GMail.com>
2018-02-26 15:31:25 -0500

    Create Listener in DrinkCategoryActivity

--- app/src/main/java/com/johnstrack/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/johnstrack/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,10 @@
 package com.johnstrack.starbuzz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,5 +17,18 @@ public class DrinkCategoryActivity extends Activity {
         ArrayAdapter<Drink> lisrAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
         ListView listDrinks = findViewById(R.id.list_drinks);
         listDrinks.setAdapter(lisrAdapter);
+
+        //Create listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listDrinks, View itemView, int position, long id) {
+                //Pass the drink the user clicks on to DrinkActivity
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+            }
+        };
+        //Assign the listener to the lsit view
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
