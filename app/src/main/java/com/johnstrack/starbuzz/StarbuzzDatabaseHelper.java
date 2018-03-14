e9ba6081efd415e270a0b50c5d040b8bb5953975
John Strack <Braveheart22@GMail.com>
2018-03-14 15:34:47 -0400

    Create the database

--- app/src/main/java/com/johnstrack/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/com/johnstrack/starbuzz/StarbuzzDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.johnstrack.starbuzz;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -20,10 +21,26 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("CREATE TABLE drink (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + "name TEXT, "
+        + "description TEXT, "
+        + "image_resource_id INTEGER);");
+
+        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(db, "Cappuccino", "Espresso, hot milk, and steamed-milk foam", R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    private static void insertDrink (SQLiteDatabase db, String name, String description, int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("name", name);
+        drinkValues.put("description", description);
+        drinkValues.put("image_resource_id", resourceId);
+        db.insert("drink", null, drinkValues);
     }
 }
