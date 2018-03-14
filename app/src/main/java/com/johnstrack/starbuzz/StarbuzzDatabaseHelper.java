96e3843bc4a2020a6f64dd0ad36be9c6f9dc1ad6
John Strack <Braveheart22@GMail.com>
2018-03-14 16:13:54 -0400

    Add new column to the drink table

--- app/src/main/java/com/johnstrack/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/com/johnstrack/starbuzz/StarbuzzDatabaseHelper.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "starbuzz";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     public StarbuzzDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -21,26 +21,39 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE drink (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        + "name TEXT, "
-        + "description TEXT, "
-        + "image_resource_id INTEGER);");
-
-        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
-        insertDrink(db, "Cappuccino", "Espresso, hot milk, and steamed-milk foam", R.drawable.cappuccino);
-        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        updateMyDatabase (db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        updateMyDatabase (db, 0, DB_VERSION);
     }
 
     private static void insertDrink (SQLiteDatabase db, String name, String description, int resourceId) {
+
         ContentValues drinkValues = new ContentValues();
         drinkValues.put("name", name);
         drinkValues.put("description", description);
         drinkValues.put("image_resource_id", resourceId);
         db.insert("drink", null, drinkValues);
+    }
+
+    private void updateMyDatabase (SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE drink (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "name TEXT, "
+                    + "description TEXT, "
+                    + "image_resource_id INTEGER);");
+
+            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk, and steamed-milk foam", R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        }
+
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE drink ADD COLUMN favorite NUMERIC;");
+        }
     }
 }
