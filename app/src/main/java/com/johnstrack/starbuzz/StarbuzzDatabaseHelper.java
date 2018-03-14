e798b98f56dd9d730bb2574a9338bb9f3478b878
John Strack <Braveheart22@GMail.com>
2018-03-14 14:50:22 -0400

    Specify the database

--- app/src/main/java/com/johnstrack/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/com/johnstrack/starbuzz/StarbuzzDatabaseHelper.java
@@ -5,12 +5,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by John on 3/14/2018 at 2:41 PM.
+ *  Created by John on 3/14/2018 at 2:41 PM.
  */
 
 public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "starbuzz";
+    private static final int DB_VERSION = 1;
+
     public StarbuzzDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
