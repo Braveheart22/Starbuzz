f66fd2c6c77ce96fc556d1fc1ab4ab1c3704ce21
John Strack <Braveheart22@GMail.com>
2018-03-19 15:54:00 -0400

    Drink activity can update the favorite column

--- app/src/main/java/com/johnstrack/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/johnstrack/starbuzz/DrinkActivity.java
@@ -1,11 +1,14 @@
 package com.johnstrack.starbuzz;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +40,7 @@ public class DrinkActivity extends Activity {
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
 
                 //Populate the drink name
                 TextView name = findViewById(R.id.name);
@@ -51,11 +55,39 @@ public class DrinkActivity extends Activity {
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
 
+                //Populate the favorite checkbox
+                CheckBox favorite = findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
+
             }
             cursor.close();
             db.close();
         } catch (SQLiteException e) {
             Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    //Update the database when the checkbox is clicked.
+    public void onFavoriteClicked (View view) {
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+
+        //Get the value of the checkbox
+        CheckBox favorite = findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("favorite", favorite.isChecked());
+
+        //Get a reference to the database and update the favorite column
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            db.update("drink",
+                    drinkValues,
+                    "_id = ?",
+                    new String[] {Integer.toString(drinkId)});
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database Unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
